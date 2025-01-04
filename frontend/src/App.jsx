a41cd2380ed782c2058978a329be4cfdfed3c85c
ArhamShameem <golushameem001@gmail.com>
2025-01-04 22:52:08 +0530

    Signup page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  return (
-    <div className='text-red-500'>Hell world!</div>
-  )
-}
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-export default App
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser) {
+    return <Loader className="size-10 animate-spin" />;
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/settings"
+            element={authUser ? <SettingsPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/profile"
+            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          />
+        </Route>
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route,Router } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>Navbar
+
+        {/* <Router>
+            
+        </Router> */}
+    </div>
+  )
+}
+
+export default Navbar
